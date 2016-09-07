235aae706a638e90e7cffbf5697ca5cee72459a4
sho kisaragi <sho@oshiire.to>
2016-09-07 18:00:46 +0900

    いったん最終形。rdoc も含む

--- check_ip_address.rb
+++ check_ip_address.rb
@@ -1,6 +1,22 @@
-def CheckIPAddress(ip)
-    # 引数は文字列のみを受け付けることとする
-    return false unless ip.is_a?(String)
+# IPアドレスのチェック
+# IPアドレスとおぼしき文字列を検査し、IPアドレスっぽいかどうかを調査
+# == パラメータ
+# ip:: チェックしたいIPアドレス文字列
+# == 返り値
+# true:: IPアドレスと思われる場合
+# false:: IPアドレスではないと思われる場合
 
-    return true
+def CheckIPAddress(ip)
+  # 引数は文字列のみを受け付けることとする
+  return false unless ip.is_a?(String)
+
+  # 正規表現で、各数値を入手
+  /^([0-9]*)\.([0-9]*)\.([0-9]*)\.([0-9]*)$/ =~ ip
+
+  return false unless (1..239).cover?($1.to_i)
+  return false unless (0..255).cover?($2.to_i)
+  return false unless (0..255).cover?($3.to_i)
+  return false unless (1..254).cover?($4.to_i)
+
+  return true
 end
--- spec/check_ip_address_spec.rb
+++ spec/check_ip_address_spec.rb
@@ -1,18 +1,64 @@
 require_relative '../check_ip_address'
 
 describe 'CheckIPAddress' do
-  describe '一つの文字列を引数とすること' do
-    context '文字列として正常に受け付けた場合' do
-      it '引数が文字列の場合は正常' do
-        expect(CheckIPAddress('test')).to eq(true)
+  describe '一つの文字列を引数として' do
+    context '文字列を受け付けた場合は' do
+      it '正常に処理が終了する' do
+        expect(CheckIPAddress('192.168.0.10')).to eq(true)
       end
     end
-    context '文字列以外を受け付けて異常となる場合' do
+    context '文字列以外を受け付けた場合' do
       it '引数が数値の場合は異常' do
         expect(CheckIPAddress(100)).to eq(false)
       end
       it '引数が配列の場合は異常' do
         expect(CheckIPAddress([100,100])).to eq(false)
+      end
+    end
+  end
+
+  describe '"."区切りで4桁で構成' do
+    context '全て"."区切りで4桁の場合' do
+      it '正常終了する' do
+        expect(CheckIPAddress('1.0.0.1')).to eq(true)
+      end
+    end
+    context 'フォーマット通りでない場合' do
+      it '区切りが"."以外の場合は異常' do
+        expect(CheckIPAddress('1:0:0:1')).to eq(false)
+        expect(CheckIPAddress('1/0/0/1')).to eq(false)
+      end
+    end
+  end
+
+  describe 'IPアドレスは1.0.0.1〜239.255.255.254のレンジ' do
+    context '指定された範囲内であれば' do
+      it '正常終了' do
+        expect(CheckIPAddress('239.255.255.254')).to eq(true)
+      end
+    end
+    context '指定された範囲外' do
+      it '一桁、四桁目が 0 は異常' do
+        expect(CheckIPAddress('0.0.0.1')).to eq(false)
+        expect(CheckIPAddress('1.0.0.0')).to eq(false)
+        expect(CheckIPAddress('0.0.0.0')).to eq(false)
+      end
+      it '一桁目が240以上は異常' do
+        expect(CheckIPAddress('240.0.0.1')).to eq(false)
+        expect(CheckIPAddress('240.255.255.254')).to eq(false)
+      end
+      it '二、三桁目が256以上は異常' do
+        expect(CheckIPAddress('0.256.0.1')).to eq(false)
+        expect(CheckIPAddress('0.0.256.1')).to eq(false)
+      end
+      it '四桁目が255以上は異常' do
+        expect(CheckIPAddress('239.255.255.255')).to eq(false)
+      end
+      it 'いずれかの桁が0より小さければ異常' do
+        expect(CheckIPAddress('-1.0.0.0')).to eq(false)
+        expect(CheckIPAddress('0.-1.0.0')).to eq(false)
+        expect(CheckIPAddress('0.0.-1.0')).to eq(false)
+        expect(CheckIPAddress('0.0.0.-1')).to eq(false)
       end
     end
   end
